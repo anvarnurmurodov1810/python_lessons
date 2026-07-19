abb2b71b5cc648c7266c4a30fc05c61c4cb2b455
Anvar Nurmurodov <anvarnurmurodov34@gmail.com>
2026-07-19 13:50:01 +0500

    new files added

---
+++ pythondars2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -#print("Iltimos quyidagi ma'lumotlarni tuldiring")
+kocha = input("kochangiz: ")
+mahalla = input("mahallangiz: ")
+tuman = input("tumaningiz: ")
+viloyat = input("viloyatingiz: ")
+
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+#print(manzil.title())
+#print(manzil.upper())
+#print(manzil.lower())
+
+print(manzil.capitalize())
---
+++ pythondars3.py
@@ -0,0 +1,43 @@
+son=int(input("istalgan butun son kiriting"))
+
+for n in range(2,100000000000000000000):     
+     
+    if not (son%n):     
+        print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
+     
+     
+     
+     
+   
+     
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+   
